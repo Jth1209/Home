6429aff7905cb98b6e043498636f5ad8c882cf81
jth1209 <xkgshand@naver.com>
2024-07-12 21:43:56 +0900

    first commit

---
+++ Java/src/Java/JavaP.java
@@ -0,0 +1,12 @@
+package Java;
+import java.util.Scanner;
+public class JavaP {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String input = sc.nextLine();
+		int val = Integer.parseInt(input);
+
+	}
+
+}
